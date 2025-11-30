bc38e3ef58e035efcd080d40872556df7b912992
sandarabelwan11-collab <sandarabelwan11@gmail.com>
2025-12-01 02:53:10 +0800

    Create Code_challenge7.py

---
+++ Code_challenge7.py
@@ -0,0 +1,11 @@
+ #ask the user to enter a number(positive, negative,or zero)
+#if statement 
+
+
+sum = 0
+for odd in range(1,10,1):
+        number = eval(input("Enter a number"))
+        print(odd)
+        sum += number
+
+print("Sum of all the odd",number,"number",sum)
