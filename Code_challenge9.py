8be37bf059f9ce41f599cc37bdc6c01e86d9f925
sandarabelwan11-collab <sandarabelwan11@gmail.com>
2025-12-01 02:54:28 +0800

    Create Code_challenge9.py

---
+++ Code_challenge9.py
@@ -0,0 +1,16 @@
+#ask the user to enter starting number
+#stimulate a simple countdown
+
+
+print("timer countdown")
+
+start_num = int(input("Enter the starting number for countdown : "))
+
+print("\ncountdown started ")
+
+for a in range(start_num, 0 ,-1):
+    print(a)
+
+print("liftoff!")
+
+
