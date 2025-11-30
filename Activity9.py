bebc541f310cad64185e031d828a246216d38223
sandarabelwan11-collab <sandarabelwan11@gmail.com>
2025-12-01 02:25:27 +0800

    Activity9.py

---
+++ Activity9.py
@@ -0,0 +1,15 @@
+a = 6
+b = 4
+
+c = 12
+d = 10
+
+print((a > b) and (c < d) )
+print((a < b) or ( c < d) )
+print( not(a < b) or (c < d))
+
+username = 'sandara'
+password = 'cutie me'
+
+print("USER LOGIN ")
+print( (username == 'darabels') and (password == 'cutie123'))
