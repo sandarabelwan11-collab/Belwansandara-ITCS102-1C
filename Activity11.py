d6c001ca88446142fc5c9d41c1f4e9d05ba15384
sandarabelwan11-collab <sandarabelwan11@gmail.com>
2025-12-01 02:27:59 +0800

    Create Activity11.py

---
+++ Activity11.py
@@ -0,0 +1,10 @@
+temp = eval(input("Enter temperature outside-->"))
+
+#input 33
+if temp >= 1 and temp <= 20:
+        print("temperature is Considered as Extremely cold")
+elif temp >= 21 and temp <= 30:
+        print("Temperature is Considered as Moderately Cold")
+else:
+        print("Invalid temperature")
+
