b3e2c577e0a3f3f13bd026bbd9f136d4cc8f6494
sandarabelwan11-collab <sandarabelwan11@gmail.com>
2025-12-01 02:26:53 +0800

    Create Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,11 @@
+name = 'pimens'
+password = 'cutie'
+
+n = input("name --> ")
+p = input("password --> ")
+
+if (n == name) and (p == password):
+        print("access granted")
+
+else:
+        print("denied")
